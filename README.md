4fae82bb0c5ae88f3281dd9bfc440de91971c57d
Pierre <pierrejouffroy974@gmail.com>
2025-09-23 20:19:03 +0200

    Update README, main.dart, and pubspec files for Anki sharing functionality

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
 # anki_app_sharing
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+allow me to share words directly to anki app via long press on them and then click share 
--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,128 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AnkiShareApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class AnkiShareApp extends StatefulWidget {
+  const AnkiShareApp({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<AnkiShareApp> createState() => _AnkiShareAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AnkiShareAppState extends State<AnkiShareApp> {
+  String? sharedText;
+  final ipController = TextEditingController(text: "172.23.240.1"); 
+  final deckController = TextEditingController(text: "Default");
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+
+    // Listen for shared text
+    ReceiveSharingIntent.getTextStream().listen((String value) {
+      setState(() {
+        sharedText = value;
+      });
+      _sendToAnki(value);
+    }, onError: (err) {
+      print("getLinkStream error: $err");
+    });
+
+    // Get initial shared text if app is launched via Share
+    ReceiveSharingIntent.getInitialText().then((String? value) {
+      if (value != null) {
+        setState(() {
+          sharedText = value;
+        });
+        _sendToAnki(value);
+      }
     });
   }
 
+  Future<void> _sendToAnki(String word) async {
+    final url = Uri.parse('http://${ipController.text}:8765');
+    final body = {
+      "action": "addNote",
+      "version": 6,
+      "key": "123Soleil",
+      "params": {
+        "note": {
+          "deckName": deckController.text,
+          "modelName": "Basic",
+          "fields": {
+            "Front": word,
+            "Back": ""
+          },
+          "options": {
+            "allowDuplicate": false
+          },
+          "tags": ["fromFlutter"]
+        }
+      }
+    };
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(body),
+      );
+
+      if (response.statusCode == 200) {
+        final res = jsonDecode(response.body);
+        _showMessage("Added: $word ✅ (id ${res['result']})");
+      } else {
+        _showMessage("Error: ${response.statusCode}");
+      }
+    } catch (e) {
+      _showMessage("Failed to connect: $e");
+    }
+  }
+
+  void _showMessage(String msg) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(msg)),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      title: 'Anki Share',
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Anki Share")),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: ipController,
+                decoration: const InputDecoration(labelText: "AnkiConnect IP"),
+              ),
+              TextField(
+                controller: deckController,
+                decoration: const InputDecoration(labelText: "Deck Name"),
+              ),
+              const SizedBox(height: 20),
+              if (sharedText != null)
+                Text("Last shared: $sharedText",
+                    style: const TextStyle(fontSize: 18)),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (sharedText != null) _sendToAnki(sharedText!);
+                },
+                child: const Text("Send Again"),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
